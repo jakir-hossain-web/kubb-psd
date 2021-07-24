fe35be000884d443cfc6a3044f785fc7c9f0f580
Jakir Hossain <83057318+jakir-hossain-web@users.noreply.github.com>
2021-07-25 00:03:45 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+// slick slider banner ============
+    $('.banner_item').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        prevArrow: '<i class="fa fa-angle-left left-arrow"></i>',
+        nextArrow: '<i class="fa fa-angle-right right-arrow"></i>',
+        });
+
+// venobox ========================
+    $('.venobox').venobox();
